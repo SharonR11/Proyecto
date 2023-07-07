3587df391a5b91d6c483c0d5733201d2cf66aa0f
SharonR11 <sharon.rudas@tecsup.edu.pe>
2023-07-07 17:29:30 -0500

    react1

--- empleo/app/src/App.js
+++ empleo/app/src/App.js
@@ -1,13 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Primercomponente from './componentes/primercomp';
+import Primercomponente from './componentes/primero';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Componente</h1>
+        <Primercomponente/>
         
       </header>
     </div>
